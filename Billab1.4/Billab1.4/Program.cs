0f620db128a4db9f72f517d95a331bcf3a73148c
Samet Ahmet Şahin <sametahmetsahin@protonmail.com>
2022-05-07 13:05:09 +0300

    1.4 and (botched) 1.5

---
+++ Billab1.4/Billab1.4/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+while (true)
+{
+    Console.WriteLine("Bir sayı giriniz:");
+    var girdi = int.Parse(Console.ReadLine());
+    if (girdi == 0) return 0;
+    var kare = Math.Pow(girdi, 2);
+    var basamaklartoplami = 0;
+    while (kare > 0)
+    {
+        basamaklartoplami += (int) kare % 10;
+        kare /= 10;
+    }
+    if (girdi == basamaklartoplami)
+    {
+        Console.WriteLine("Eşit");
+    }
+    else
+    {
+        Console.WriteLine(Math.Pow(girdi, 2) + " Eşit değil");
+    }
+}
---
+++ Billab1.5/Billab1.5/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// Botched this up. Might reattempt later.
+
+Console.WriteLine("Matrisin satir sayisini giriniz:");
+var satir = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Matrisin sütun sayisini giriniz:");
+var sutun = int.Parse(Console.ReadLine());
+int sira = 0;
+
+for (int i=0;i<satir;i++)
+{
+    for (int j=0;j<sutun;j++)
+    {
+        sira++;
+        Console.Write(sira + " ");
+        
+    }
+    Console.Write("\n");
+}
